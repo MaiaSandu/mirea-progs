ca0efc22cf83f999b59f6c95f2974d115714406b
MaiaSandu <121349696+MaiaSandu@users.noreply.github.com>
2022-12-25 23:05:19 +0300

    Add files via upload

---
+++ julia_my_love_forever/8/8_test.jl
@@ -0,0 +1,4 @@
+using HorizonSideRobots
+include("8.jl")
+r = Robot("untitled.sit", animate = true)
+find_the_marker!(r)
---
+++ julia_my_love_forever/9/9_test.jl
@@ -0,0 +1,4 @@
+using HorizonSideRobots
+include("10.jl")
+r = Robot("untitled.sit", animate = true)
+chess_order_marker!(r)
