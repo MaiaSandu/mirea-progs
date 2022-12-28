bdccf6ad8ded627482bec5f5648439abf5084ee8
MaiaSandu <121349696+MaiaSandu@users.noreply.github.com>
2022-12-29 00:43:59 +0300

    Update 20_test.jl

--- julia_my_love_forever/20/20_test.jl
+++ julia_my_love_forever/20/20_test.jl
@@ -1,4 +1,4 @@
 using HorizonSideRobots
-include("20.jl")
-r = Robot("untitled.sit", animate=true)
-get_on_through_rec!(r, Nord)
+include("cjeicnuirc.jl")
+r = Robot("untitled.sit", animate = true)
+next_kletka!(r, Nord)
