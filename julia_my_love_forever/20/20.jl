a58c3aee89b93e432c7d47258862d3089d7bfc45
MaiaSandu <121349696+MaiaSandu@users.noreply.github.com>
2022-12-29 00:43:42 +0300

    Update 20.jl

--- julia_my_love_forever/20/20.jl
+++ julia_my_love_forever/20/20.jl
@@ -1,27 +1,12 @@
-function get_on_through_rec!(r::Robot, side::HorizonSide, n_steps::Int = 0)
-    if isborder(r, side)
-        move!(r, next_side(side))
-        n_steps += 1
-        get_on_through_rec!(r, side, n_steps)
-    else
-        move!(r, side)
-        along!(r, inverse_side(next_side(side)), n_steps)
-    end
-end
-
-function next_side(side::HorizonSide)::HorizonSide
-    return HorizonSide((Int(side) + 1) % 4)
-end
-
-function along!(robot, side, num_steps)
-    n_steps = 0
-    while !isborder(robot, side) && n_steps < num_steps
+function next_kletka!(robot, side)
+    if !isborder(robot, side)
         move!(robot, side)
-        n_steps += 1
+    else
+        move!(robot, right(side))
+        next_kletka!(robot, side)
+        move!(robot, left(side))
     end
 end
 
-function inverse_side(side::HorizonSide)::HorizonSide
-    inv_side = HorizonSide((Int(side) + 2) % 4)
-    return inv_side
-end
+right(side::HorizonSide) = HorizonSide((Int(side) +1)% 4)
+left(side::HorizonSide) = HorizonSide((Int(side) +3)% 4)
