5f3e75480426b4e6cedff6dfedafe92db769f1f9
MaiaSandu <121349696+MaiaSandu@users.noreply.github.com>
2022-12-28 20:03:45 +0300

    Update 8.jl

--- julia_my_love_forever/8/8.jl
+++ julia_my_love_forever/8/8.jl
@@ -3,10 +3,20 @@ function find_the_marker!(r)
     side = Sud
     while !ismarker(r)
         for _i  in 1:2
-            move!(r, side)
+            the_marker_is_found!(r, side, num_steps)
             side = next(side)
         end
         num_steps += 1
+    end
+end
+
+function the_marker_is_found!(r, side, num_steps)
+    for _i in 1:num_steps
+        if !ismarker(r)
+            move!(r, side)
+        else
+            break
+        end
     end
 end
 
