1780245436709e1f2a50eb3c315cdf5631bb09da
MaiaSandu <121349696+MaiaSandu@users.noreply.github.com>
2022-12-28 19:55:54 +0300

    Update 8.jl

--- julia_my_love_forever/8/8.jl
+++ julia_my_love_forever/8/8.jl
@@ -1,22 +1,13 @@
-function find_the_marker!(robot)
-    num_step=1
-    side=Nord
-    while !ismarker(robot)
-        for _i in 1:2   
-            marker_is_find!(robot ,side, num_step)
-            side=next(side)
+function find_the_marker!(r)
+    num_steps = 1
+    side = Sud
+    while !ismarker(r)
+        for _i  in 1:2
+            move!(r, side)
+            side = next(side)
         end
-        num_step=num_step+1
+        num_steps += 1
     end
 end
 
-function marker_is_find!(robot, side, num_step)  
-    for _i in 1:num_step   
-        if !ismarker(r)
-            move!(robot,side)
-        else
-            break
-        end 
-    end
-end
 next(side::HorizonSide) = HorizonSide((Int(side) +1)% 4)
